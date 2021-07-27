d207a0d73c6bf732603b76de6a6671abc84acca3
Ben Kolligs <bkolligs@andrew.cmu.edu>
2021-07-26 22:23:31 -0400

    Added description of the incidence list assembly algorithm
    added some images to illustrate the tree
    
    Added tests requested by pull request

--- robotics/tests/test_kinematic_tree.py
+++ robotics/tests/test_kinematic_tree.py
@@ -54,3 +54,55 @@ def test_root_comparison(fixture):
 def test_root_arbitrary_frame(fixture):
 	grounded = fixture.root(destination='link2')
 	assert grounded['link5'] == robotics.Transform(0.1, 0.5, -0.7, np.pi, 0, 0, parent='link2', child='link5') == fixture.get('link2', 'link5')
+
+@pytest.fixture
+def new_name_fixture():
+	'''
+	For testing a tree with a base link named something new
+	'''
+	base_link = robotics.Transform(name="rock")
+	hand = robotics.Transform(x=0.5, parent="rock", child="hand", name="hand")
+	box = robotics.Transform(x=1.0, z=0.5, parent="hand", child="box", name="box")
+	tree = robotics.KinematicTree([base_link, hand, box], root_name="rock")
+	return tree
+
+def test_new_name(new_name_fixture):
+	grounded = new_name_fixture.root()
+	assert len(grounded) == len(new_name_fixture.tree_rep)
+	for g in grounded:
+		assert grounded[g].parent == 'rock'
+
+def test_new_name_compare(new_name_fixture):
+	path = new_name_fixture.get('rock', 'box')
+	compare = robotics.Transform(x=1.5, z=0.5, parent='rock', child='box')
+	assert compare == path
+	path = new_name_fixture.get('box', 'rock')
+	compare = robotics.Transform(x=-1.5, z=-0.5, parent='box', child='rock')
+	assert compare == path
+
+@pytest.fixture
+def complicated():
+	chassis = robotics.Transform(name="chassis")
+	link1 = robotics.Transform(0.125, -0.5, -0.5, 0, 0, np.pi/3, parent="chassis", child="link1", name="bTo1")
+	link2 = robotics.Transform(0.125, 0.5, -0.5, 0, 0, -np.pi/3, parent="chassis", child="link2", name="bTo2")
+	link3 = robotics.Transform(0.0, 0.0, -0.2, np.pi, 0.5426, 0.6392, parent="link1", child="link3", name="3To1")
+	link4 = robotics.Transform(1.2643, -0.5, -1.0, 0.0, 0.523, 0, parent="link3", child="link4", name="4To3")
+	link5 = robotics.Transform(1.2643, 0.5, -1.0, 0.0, 1.532, 0, parent="link3", child="link5", name="5To3")
+	tree = robotics.KinematicTree([chassis, link1, link2, link3, link4, link5], root_name='chassis')
+	return {'tree': tree, 'link1' : link1, 'link2' : link2, 'link3' : link3, 'link4':link4, 'link5':link5}
+
+def test_complex_traversal(complicated):
+	forward = complicated['tree'].get('link5', 'link2')
+	backward = complicated['tree'].get('link2', 'link5')
+	inv = forward * backward
+	assert np.allclose(inv.transform, np.eye(4))
+
+def test_complex_root(complicated):
+	grounded = complicated['tree'].root()
+	for g in grounded:
+		assert grounded[g].parent == 'chassis'
+	
+	test = grounded['link5']
+	multiply = complicated['link1'] * complicated['link3'] * complicated['link5']
+	get = complicated['tree'].get('chassis', 'link5')
+	assert test == get == multiply
