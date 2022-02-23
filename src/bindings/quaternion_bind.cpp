05efad84bb04d4b344c3502fccce0971d7d5bffd
Ben Kolligs <benkolligs@gmail.com>
2022-02-23 17:22:46 -0500

    Added the vector rotation binding

--- src/bindings/quaternion_bind.cpp
+++ src/bindings/quaternion_bind.cpp
@@ -2,18 +2,19 @@
 #include "quaternion.h"
 
 void quaternion_bind(py::module_ &m) {
-	py::class_<Quaternion>(m, "Quaternion")
+	py::class_<Quaternion>(m, "_Quaternion")
 		.def(py::init<double, double, double, double>(),
 			"w"_a=0, "x"_a=0, "y"_a=0, "z"_a=0)
-		.def("w"          , &Quaternion::w)
-		.def("x"          , &Quaternion::x)
-		.def("y"          , &Quaternion::y)
-		.def("z"          , &Quaternion::z)
-		.def("data"       , &Quaternion::data)
-		.def("inv"        , &Quaternion::inv)
-		.def("norm"       , &Quaternion::norm)
-		.def("adjoint"    , &Quaternion::adjoint)
-		.def("approx"     , &Quaternion::isApprox)
+		.def(py::init<Eigen::Vector3d>())
+		.def("_w"          , &Quaternion::w)
+		.def("_x"          , &Quaternion::x)
+		.def("_y"          , &Quaternion::y)
+		.def("_z"          , &Quaternion::z)
+		.def("_data"       , &Quaternion::data)
+		.def("_inv"        , &Quaternion::inv)
+		.def("_norm"       , &Quaternion::norm)
+		.def("_adjoint"    , &Quaternion::adjoint)
+		.def("_approx"     , &Quaternion::isApprox)
 		.def("__repr___"  , &Quaternion::toRepr)
 		.def("__str__"    , &Quaternion::toString)
 		.def(double() * py::self)
@@ -28,5 +29,9 @@ void quaternion_bind(py::module_ &m) {
 		.def(double() / py::self)
 		.def(py::self / double())
 		.def(py::self / py::self)
+		// Define the multiplication overloading for a vector
+		.def("__mul__", [](Quaternion&q, Eigen::Vector3d & v){
+			return q*v;
+		})
 		;
 }
